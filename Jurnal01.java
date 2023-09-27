48ac879c1d5065f81d06a188854da2a68ec3a988
RickardoSatrioo <rickardosatrio@gmail.com>
2023-09-27 20:57:24 +0700

    jurnal

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args){
+        System.out.println("NAMA:RICKARDO SATRIO");
+        System.out.println("NIM:67062300051");
+        System.out.println("KELAS:D3IF-47-04");
+    }
+}
